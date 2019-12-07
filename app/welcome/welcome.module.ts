0413bae490a71121951fd1827116b6c268c5483e
gilles <gillespockpock@hotmail.fr>
2019-12-07 22:09:15 +0100

    pages

--- app/welcome/welcome.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { WelcomeRoutingModule } from './welcome-routing.module';
-
-@NgModule({
-  imports:      [ WelcomeRoutingModule ],
-  declarations: [ WelcomeRoutingModule.components ]
-})
-export class WelcomeModule { }
